7d8184dd1a17fc58710a3be2cce081945119a63f
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-05-28 00:20:20 +0300

    add: readme to L-2, edit app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+const links = [
+	'Homework #1',
+	'Homework #2',
+	'Homework #3',
+	'Homework #4',
+	'Homework #5',
+	'Homework #6',
+	'Homework #7',
+	'Homework #8',
+];
+const res = links.map((element, index) => (
+	<li key={index}>
+		<a className="lesson-link" href={`/lesson-${index + 1}`}>
+			{element}
+		</a>
+	</li>
+));
+console.log(res);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Aston React Intesive</h1>
+			<nav>
+				<ul className="lesson-links">{res}</ul>
+			</nav>
+		</div>
+	);
 }
 
 export default App;
